b99a00e914964a0bbe7b35859a3601152f802d1f
Pablo Palazon <pablocryystalz24@gmail.com>
2025-01-15 08:28:58 +0100

    Controlador de recursos de ciclos terminado

--- app/Models/Ciclo.php
+++ app/Models/Ciclo.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ciclo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'codCiclo',
+        'codFamilia',
+        'familia_id',
+        'grado',
+        'nombre'
+    ];
+    
 }
